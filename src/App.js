fd622664d7e261b45ad5591cd6d6c77d4e256d2f
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-09-23 11:57:01 +0900

    state 변경

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [글제목, 글제목변경] = useState("남자 코트 추천");
-  let [글제목2, 글제목변경2] = useState("남자 코트 추천2");
+  // let [글제목, 글제목변경] = useState("남자 코트 추천");
+  // let [글제목2, 글제목변경2] = useState("남자 코트 추천2");
   // let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동 맛집"]);
-  let posts = "분당 맛집";
+  // let posts = "분당 맛집";
 
-  let [제목, 제목변경] = useState(["NodeJS", "ReactJS", "Django"]);
+  let [제목, 제목변경] = useState(["NodeJS", "ReactJS", "Django", "Python"]);
+
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
@@ -17,7 +19,20 @@ function App() {
       </div>
 
       <div className="list">
-        <h3> {제목[0]} </h3>
+        <h3>
+          {제목[0]}
+          <span
+            // onClick={() => {
+            //   console.log('1');
+            // }}
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>
+          {따봉}
+        </h3>
         <p>9월 22일 발행</p>
         <hr />
       </div>
